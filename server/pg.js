2bd3d1f4d2a90e5640ff25ed802780b0b9a9b7eb
Jaybur1 <jayburbyga@gmail.com>
2020-04-05 18:31:01 -0400

    master merge

--- server/pg.js
+++ server/pg.js
@@ -15,6 +15,7 @@ module.exports = function() {
       port:     process.env.DB_PORT,
       user:     process.env.DB_USER,
       password: process.env.DB_PASS,
+      ssl:      process.env.DB_SSL,
       database: process.env.DB_NAME
     };
   }
