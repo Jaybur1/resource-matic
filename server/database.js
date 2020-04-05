175cd67a753ef6ea610747dc6b0b7f65dbc25656
aliyasser20 <aliyasser20@hotmail.com>
2020-04-05 13:32:00 -0400

    testing all possibilities for get resources

--- server/database.js
+++ server/database.js
@@ -86,18 +86,16 @@ const getResources = (db, options) => {
 
   // ? SELECT section of query
   // Initialize query
-  let queryString = `
-    SELECT resources.*
-  `;
+  let queryString = `SELECT resources.*`;
 
   // Comments are requested
-  options.comments ? queryString += `, comments.message AS comments` : null;
+  options.comments ? queryString += `, comments.body AS comments` : null;
 
   // Likes are requested
   options.likes ? queryString += `, count(likes) AS likes` : null;
 
   // Average ratings are requested
-  options.avgRatings ? queryString += `, average(ratings.rating) AS avgRatings` : null;
+  options.avgRatings ? queryString += `, avg(ratings.rating) AS avgRatings` : null;
 
   // All ratings are requested
   options.ratings ? queryString += `, ratings.rating AS rating` : null;
@@ -106,24 +104,28 @@ const getResources = (db, options) => {
   options.users || options.currentUser ? queryString += `, users.name AS users` : null;
 
   // Categories are requested
-  options.categories.length > 0 ? queryString += `, categories.name AS categories` : null;
+  options.categories ? queryString += `, categories.name AS categories` : null;
+  // ?
+  
+  // ? FROM section of query
+  queryString += ` FROM resources `;
   // ?
 
   // ? JOIN section of query
   // Comments are requested
-  options.comments ? queryString += `JOIN comments ON comments.resource_id = resources.id` : null;
+  options.comments ? queryString += `JOIN comments ON comments.resource_id = resources.id ` : null;
 
   // Likes are requested
-  options.likes ? queryString += `JOIN likes ON likes.resource_id = resources.id` : null;
+  options.likes ? queryString += `JOIN likes ON likes.resource_id = resources.id ` : null;
 
   // Average ratings or all ratings are requested
-  options.avgRatings || options.ratings ? queryString += `JOIN ratings ON ratings.resource_id = resources.id` : null;
+  options.avgRatings || options.ratings ? queryString += `JOIN ratings ON ratings.resource_id = resources.id ` : null;
 
   // Users or current user are requested
-  options.users || options.currentUser ? queryString += `JOIN users ON resources.user_id = users.id` : null;
+  options.users || options.currentUser ? queryString += `JOIN users ON resources.user_id = users.id ` : null;
   
   // Categories are requested
-  options.categories.length > 0 ? queryString += `JOIN categories ON resources.category_id = categories.id` : null;
+  options.categories ? queryString += `JOIN categories ON resources.category_id = categories.id ` : null;
   // ?
   
 
@@ -131,131 +133,121 @@ const getResources = (db, options) => {
   // Current user is requested
   if (options.currentUser) {
     queryParams.push(`${options.currentUser}`);
-    queryString += `WHERE users.id = $${queryParams.length}`;
+    queryString += ` WHERE users.name = $${queryParams.length} `;
   }
   
   // Categories requested
-  if (options.categories.length > 0) {
+  if (options.categories) {
     // Can be filtered by multiple categories
     options.categories.forEach(category => {
       queryParams.push(`%${category}%`);
-      queryString += `AND categories.name LIKE $${queryParams.length}`;
+      queryString += ` AND categories.name LIKE $${queryParams.length} `;
     });
   }
   // ?
   
+  // ? GROUP BY section of query
+  let alreadyGrouped = false;
+
+  // Likes are requested
+  if (options.likes) {
+    queryString += ` GROUP BY resources.id`;
+    alreadyGrouped = true;
+  }
+  
+  if (options.comments) {
+    alreadyGrouped ? queryString += `, ` : queryString += ` GROUP BY `;
+    queryString += `comments.body`;
+    alreadyGrouped = true;
+  }
+  
+  if (options.ratings) {
+    alreadyGrouped ? queryString += `, ` : queryString += ` GROUP BY `;
+    queryString += `ratings.rating `;
+    alreadyGrouped = true;
+  }
+  // ?
+
+  // if (options.minimum_rating) {
+  //   queryParams.push(options.minimum_rating);
+  //   queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  // }
+  
   // ? ORDER BY section of query
   // Any sorts needed
-  if (Object.keys(options.sort).length) {
-    queryString += `ORDER BY `;
+  if (options.sorts) {
+    queryString += ` ORDER BY `;
     
     let alreadySorted = false;
 
     // Sort by latest requested
-    if (options.byLatest) {
+    if (options.sorts.byLatest) {
       queryString += `resources.created DESC`;
       alreadySorted = true;
     }
     
     // Sort by oldest requested
-    if (options.byOldest) {
+    if (options.sorts.byOldest) {
       alreadySorted ? queryString += `, ` : null;
       queryString += `resources.created ASC`;
       alreadySorted = true;
     }
     
-    // Sort by highest rating requested
-    if (options.byHighestRating) {
+    // Sort by highest average rating requested
+    if (options.sorts.byHighestRating) {
       alreadySorted ? queryString += `, ` : null;
-      queryString += `ratings.rating DESC`;
+      queryString += `avg(ratings.rating) DESC`;
       alreadySorted = true;
     }
     
-    // Sort by lowest rating requested
-    if (options.byLowestRating) {
+    // Sort by lowest average rating requested
+    if (options.sorts.byLowestRating) {
       alreadySorted ? queryString += `, ` : null;
-      queryString += `ratings.rating ASC`;
+      queryString += `avg(ratings.rating) ASC`;
       alreadySorted = true;
     }
     
     // Sort by most popular requested
-    if (options.byMostPopular) {
+    if (options.sorts.byMostPopular) {
       alreadySorted ? queryString += `, ` : null;
       queryString += `likes DESC`;
       alreadySorted = true;
     }
     
     // Sort by least popular requested
-    if (options.byMostPopular) {
+    if (options.sorts.byLeastPopular) {
       alreadySorted ? queryString += `, ` : null;
       queryString += `likes ASC`;
       alreadySorted = true;
     }
   }
-
+  // ?
   
-
+  // ? Limit section of query
+  queryParams.push(options.limit || 10);
+  queryString += ` LIMIT $${queryParams.length};`;
   // ?
 
+  console.log(queryString);
 
-
-
-  // let queryString = `
-  // SELECT resources.*, avg(property_reviews.rating) as average_rating
-  // FROM properties
-  // JOIN property_reviews ON properties.id = property_id
-  // `;
-
-  // 3
-  // if (Object.keys(options).length > 0) {
-  //   queryString += "WHERE ";
-  // }
-
-  // if (options.city) {
-  //   queryParams.push(`%${options.city}%`);
-  //   queryString += `city LIKE $${queryParams.length} `;
-  // }
-
-  // if (options.owner_id) {
-  //   queryParams.push(options.owner_id);
-  //   queryParams.indexOf(options.owner_id) > 0 ? queryString += " AND " : null;
-  //   queryString += `properties.owner_id = $${queryParams.length} `;
-  // }
-
-  // if (options.minimum_price_per_night) {
-  //   queryParams.push(options.minimum_price_per_night * 100);
-  //   queryParams.indexOf(options.minimum_price_per_night * 100) > 0 ? queryString += " AND " : null;
-  //   queryString += `properties.cost_per_night >= $${queryParams.length} `;
-  // }
-
-  // if (options.maximum_price_per_night) {
-  //   queryParams.push((options.maximum_price_per_night * 100));
-  //   queryParams.indexOf(options.maximum_price_per_night * 100) > 0 ? queryString += " AND " : null;
-  //   queryString += `properties.cost_per_night <= $${queryParams.length} `;
-  // }
-
-  // queryString += `
-  // GROUP BY properties.id
-  // `;
-
-  // if (options.minimum_rating) {
-  //   queryParams.push(options.minimum_rating);
-  //   queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
-  // }
-
-  // // 4
-  // queryParams.push(limit);
-  // queryString += `
-  // ORDER BY cost_per_night
-  // LIMIT $${queryParams.length};
-  // `;
-
-  // 5
-  // console.log(queryString, queryParams);
-
-  // 6
   // return queryExecute(queryString, queryParams, (rows) => rows);
 };
 // ! ---------------------------------------------------------------------------------------- //
+
+// getResources(null, {});
+// getResources(null, {currentUser: "Ali"});
+// getResources(null, {currentUser: "Ali", comments: true});
+// getResources(null, {comments: true});
+// getResources(null, {comments: true, likes: true, ratings: true});
+// getResources(null, {likes: true, ratings: true});
+// getResources(null, {likes: true, avgRatings: true});
+// getResources(null, {likes: true, avgRatings: true, sorts: {byHighestRating: true}});
+// getResources(null, {likes: true, avgRatings: true, sorts: {byLowestRating: true}});
+// getResources(null, {likes: true, avgRatings: true, sorts: {byMostPopular: true}});
+// getResources(null, {likes: true, avgRatings: true, sorts: {byLeastPopular: true}});
+// getResources(null, {likes: true, avgRatings: true, sorts: {byLatest: true}});
+// getResources(null, {likes: true, avgRatings: true, sorts: {byOldest: true}});
+// getResources(null, {likes: true, avgRatings: true, sorts: {byOldest: true, byLeastPopular: true}});
+// getResources(null, {likes: true, avgRatings: true, sorts: {byOldest: true, byMostPopular: true}});
 
 module.exports = { getUserWithEmail, getUserWithId, addUser, updateUser, updateUserWithCreds, validatePassword, getResources };
