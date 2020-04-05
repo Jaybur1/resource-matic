399c245d131b946406d8280c5e542c172858a980
Jaybur1 <jayburbyga@gmail.com>
2020-04-05 13:47:32 -0400

    code cleaning userHelpers

--- public/scripts/userHelpers.js
+++ public/scripts/userHelpers.js
@@ -1,74 +1,87 @@
+// userHelpers.js
+//
+// user authentication support.
+
+
 const handleError = (data) => {
-  console.log(data)
-  $('body')
-  .toast({
-    class:'error',
-    title: 'Error',
+  console.log(data);
+  $("body").toast({
+    class: "error",
+    title: "Error",
     message: data.err,
-    showProgress: 'bottom',
-    showIcon: 'fire',
+    showProgress: "bottom",
+    showIcon: "fire",
   });
-}
-
-
-
+};
 
 const logOut = () => {
   return $.ajax({
     method: "PUT",
     url: "/users/logout",
-    success: (data,textStatus) => {
-      window.location.href = data.redirect
-    }
+    success: (data, textStatus) => {
+      window.location.href = data.redirect;
+    },
   });
-}
+};
 
-const logIn = data =>  {
+const logIn = (data) => {
   return $.ajax({
     method: "PUT",
     url: "/users/login",
     data,
-    success: (data,textStatus) => {
-      if(data.redirect) {
-        window.location.href = data.redirect
-      }else {
-       handleError(data)
+    success: (data, textStatus) => {
+      if (data.redirect) {
+        window.location.href = data.redirect;
+      } else {
+        handleError(data);
       }
-    }
+    },
   });
-}
+};
 
-const signUp = data => {
+const signUp = (data) => {
   return $.ajax({
     method: "POST",
     url: "/users",
     data,
-    success: (data,textStatus) => {
-      if(data.redirect) {
-        window.location.href = data.redirect
-      }else {
-       handleError(data)
+    success: (data, textStatus) => {
+      if (data.redirect) {
+        window.location.href = data.redirect;
+      } else {
+        handleError(data);
       }
-    }
+    },
   });
-}
+};
 
-$(document).ready(()=>{
-$('.login-form').on('submit',(e)=> {
-  e.preventDefault();
-  const user_input = {email: $('.login-email').val(), password:$('.login-password').val()}
-  logIn(user_input)
-})
+//handle user auth
 
-$('.logout-btn').on('submit',(e) => {
-  e.preventDefault()
-  logOut();
-})
+const userAuth = () => {
+  $(".login-form").on("submit", (e) => {
+    e.preventDefault();
+    const user_input = {
+      email: $(".login-email").val(),
+      password: $(".login-password").val(),
+    };
+    logIn(user_input);
+  });
 
-$('.signup-form').on('submit',(e)=> {
-  e.preventDefault();
-  const user_input = {name:$('.signup-name').val() ,email: $('.signup-email').val(), password:$('.signup-password').val()}
-  signUp(user_input)
-})
+  $(".logout-btn").on("submit", (e) => {
+    e.preventDefault();
+    logOut();
+  });
 
+  $(".signup-form").on("submit", (e) => {
+    e.preventDefault();
+    const user_input = {
+      name: $(".signup-name").val(),
+      email: $(".signup-email").val(),
+      password: $(".signup-password").val(),
+    };
+    signUp(user_input);
+  });
+};
+
+$(document).ready(() => {
+  userAuth();
 });
