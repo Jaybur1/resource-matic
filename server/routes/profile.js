d15a68978363028c2bd3b5860883d95c54836d7e
d0ugr <doug@escm.ml>
2020-04-05 11:07:49 -0400

    Redirect if no user session

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -20,65 +20,74 @@ module.exports = (db) => {
   //    Render the user profile page.
 
   router.get("/", (req, res) => {
-    getUserWithId(req.session.userId, db
-    ).then((user) => {
-      console.log(user);
-      res.render("profile", {
-        APP_NAME: "tSyn",
-        user: {
-          email:  user.email,
-          name:   user.name,
-          avatar: user.avatar
-        }
+    if (req.session.userId) {
+      getUserWithId(req.session.userId, db
+      ).then((user) => {
+        console.log(user);
+        res.render("profile", {
+          APP_NAME: "tSyn",
+          user: {
+            email:  user.email,
+            name:   user.name,
+            avatar: user.avatar
+          }
+        });
+      }).catch((_err) => {
+        //console.log("getUserWithId failed:", err);
+        res.redirect("/");
       });
-    }).catch((err) => {
-      console.log(err);
-      res.status(500).end();
-    });
+    } else {
+      res.redirect("/");
+    }
   });
 
   // PUT profile
   //    Update user info.
 
   router.put("/", (req, res) => {
-    //console.log(req.body);
-    const user = req.body;
-    // Save stuff that doesn't require a password:
-    if (!user.password) {
-      //console.log("Saving name, avatar...", user);
-      updateUser(db, [ user.name, user.avatar, req.session.userId ]
-      ).then(function(_updateRes) {
-        //console.log(_updateRes.rows);
-        //res.status(200).end();
-        res.redirect(303, "/home");
-      }).catch(function(err) {
-        console.log(err);
-        res.status(500).end();
-      });
-    // Check the password if changing login info:
-    } else {
-      //console.log("Saving name, avatar, email, password...");
-      validatePassword(db, req.session.userId, user.password
-      ).then(function() {
-        bcrypt.hash(user.newPassword, SALT_ROUNDS
-        ).then(function(pwHash) {
-          updateUserWithCreds(db, [ user.email, pwHash, user.name, user.avatar, req.session.userId ]
-          ).then(function(_updateRes) {
-            //console.log(_updateRes.rows);
-            //res.status(200).end();
-            res.status(200).end();
+    if (req.session.userId) {
+      //console.log(req.body);
+      const user = req.body;
+      // Save stuff that doesn't require a password:
+      if (!user.password) {
+        //console.log("Saving name, avatar...", user);
+        updateUser(db, [ user.name, user.avatar, req.session.userId ]
+        ).then(function(_updateRes) {
+          //console.log(_updateRes.rows);
+          //res.status(200).end();
+          res.redirect(303, "/home");
+        }).catch(function(err) {
+          console.log(err);
+          res.status(500).end();
+        });
+      // Check the password if changing login info:
+      } else {
+        //console.log("Saving name, avatar, email, password...");
+        validatePassword(db, req.session.userId, user.password
+        ).then(function() {
+          bcrypt.hash(user.newPassword, SALT_ROUNDS
+          ).then(function(pwHash) {
+            updateUserWithCreds(db, [ user.email, pwHash, user.name, user.avatar, req.session.userId ]
+            ).then(function(_updateRes) {
+              //console.log(_updateRes.rows);
+              //res.status(200).end();
+              res.status(200).end();
+            }).catch(function(err) {
+              console.log(err);
+              res.status(500).end();
+            });
           }).catch(function(err) {
-            console.log(err);
+            console.log("bcrypt hash failed\n", err);
             res.status(500).end();
           });
         }).catch(function(err) {
-          console.log("bcrypt hash failed\n", err);
-          res.status(500).end();
+          console.log("validatePassword failed\n", err);
+          res.status(403).end();
         });
-      }).catch(function(err) {
-        console.log("validatePassword failed\n", err);
-        res.status(403).end();
-      });
+      }
+    } else {
+      console.log("PUT /profile failed: No session");
+      res.status(303).end();
     }
   });
 
