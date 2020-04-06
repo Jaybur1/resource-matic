8169f4655c4cfb7e997ad395cdbeab7222af1497
Jaybur1 <jayburbyga@gmail.com>
2020-04-06 17:17:58 -0400

    new-resource.j organize

--- public/scripts/new-resource.js
+++ public/scripts/new-resource.js
@@ -11,12 +11,18 @@ const getCategories = () => {
     },
   });
 };
-const categorySelect = () => {
-  // $(".category-select").on("click", () => {
-    
 
-  // });
-};
+const updateCategoryList = () => {
+  getCategories().then((data) => {
+    let categoryList = "";
+    data.forEach((obj) => {
+      categoryList += `<option class='item' value=${obj.id}>${obj.name}</option>`;
+    });
+    return categoryList
+  }).then(list => {
+    $(".category-menu").html(list);
+  })
+}
 
 const newResourceCall = (data) => {
   return $.ajax({
@@ -41,26 +47,19 @@ const handleThumbnail_photo = () => {
 
 // Function that loads new resource interactions
 const newResourceHendler = () => {
-  //toggle create new resource on click
-  categorySelect();
   handleThumbnail_photo();
+   
   $(".create-new-resource").on("click", function () {
-
-    getCategories().then((data) => {
-      let categoryList = "";
-      data.forEach((obj) => {
-        categoryList += `<option class='item' value=${obj.id}>${obj.name}</option>`;
-      });
-      return categoryList
-    }).then(list => {
-      $(".category-menu").html(list);
-    })
-
-
+    //update category list acording to db
+    updateCategoryList();
+   //toggle create new resource
     $(".ui.modal").modal("show");
+
     $(".cancel-form").on("click", () => {
       $(".ui.modal").modal("hide");
     });
+
+    //handle submit new resource
     $(".new-resource-form").on("submit", function (e) {
       e.preventDefault();
       const newResourceInput = $(this).serializeArray();
@@ -69,8 +68,9 @@ const newResourceHendler = () => {
         objdata[obj.name] = obj.value;
       });
       objdata.thumbnail_photo = `https://api.faviconkit.com/${objdata.content}/144`;
-      console.log(objdata)
+
       newResourceCall(objdata);
+      //hide on submition
       $(".ui.modal").modal("hide");
     });
   });
