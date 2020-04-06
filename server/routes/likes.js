020b13c408099981d4c63124525a05780847924a
d0ugr <doug@escm.ml>
2020-04-06 12:34:26 -0400

    Implement GET /like

--- server/routes/likes.js
+++ server/routes/likes.js
@@ -16,7 +16,19 @@ module.exports = (db) => {
 
   router.get("/", (req, res) => {
     if (req.session.userId) {
-
+      const resourceID = req.query.resourceID;
+      if (resourceID) {
+        //db.query("SELECT COUNT(likes), emoji_id FROM likes WHERE resource_id = $1 GROUP BY emoji_id", [ req.body.resourceID ])
+        db.query("SELECT COUNT(*) FROM likes WHERE resource_id = $1", [ resourceID ])
+          .then((queryRes) => {
+            console.log(queryRes);
+            res.status(200).json(queryRes.rows[0]);
+          }).catch((err) => {
+            util.httpError("GET /like failed:", err, res, 500);
+          });
+      } else {
+        util.httpError("GET /like failed:", "Resource ID not specified", res, 400);
+      }
     } else {
       util.httpError("GET /like failed:", "No session", res, 403);
     }
