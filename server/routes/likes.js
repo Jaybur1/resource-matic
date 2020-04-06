f7a26141db2c1052090daf1be9d01ab228bfeb44
d0ugr <doug@escm.ml>
2020-04-06 13:09:51 -0400

    Implement DELETE /like

--- server/routes/likes.js
+++ server/routes/likes.js
@@ -21,7 +21,6 @@ module.exports = (db) => {
         //db.query("SELECT COUNT(likes), emoji_id FROM likes WHERE resource_id = $1 GROUP BY emoji_id", [ req.body.resourceID ])
         db.query("SELECT COUNT(*) FROM likes WHERE resource_id = $1", [ resourceID ])
           .then((queryRes) => {
-            console.log(queryRes);
             res.status(200).json(queryRes.rows[0]);
           }).catch((err) => {
             util.httpError("GET /like failed:", err, res, 500);
@@ -40,11 +39,10 @@ module.exports = (db) => {
   router.post("/", (req, res) => {
     if (req.session.userId) {
       db.query("INSERT INTO likes (user_id, resource_id) VALUES ($1, $2)", [ req.session.userId, req.body.resourceID ])
-        .then((queryRes) => {
-          console.log(queryRes);
+        .then((_queryRes) => {
           res.status(200).end();
         }).catch((err) => {
-          util.httpError("GET /like failed:", err, res, 500);
+          util.httpError("POST /like failed:", err, res, 500);
         });
     } else {
       util.httpError("POST /like failed:", "No session", res, 403);
@@ -56,7 +54,12 @@ module.exports = (db) => {
 
   router.delete("/", (req, res) => {
     if (req.session.userId) {
-
+      db.query("DELETE FROM likes WHERE user_id = $1 AND resource_id = $2", [ req.session.userId, req.body.resourceID ])
+        .then((_queryRes) => {
+          res.status(200).end();
+        }).catch((err) => {
+          util.httpError("DELETE /like failed:", err, res, 500);
+        });
     } else {
       util.httpError("DELETE /like failed:", "No session", res, 403);
     }
