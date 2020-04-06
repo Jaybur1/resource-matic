e5d53bd1089fe4ce3d8fbbf093f2abe2486506f5
d0ugr <doug@escm.ml>
2020-04-05 23:15:18 -0400

    Disable eslint bitching for now

--- server/util.js
+++ server/util.js
@@ -47,6 +47,8 @@ const httpError = function(logMessage, err, res, httpStatus) {
 // validateEmailFormat checks to see if a string contains a valid email address.
 
 const validateEmailFormat = function(email) {
+  // Not sure if necessary:
+  // eslint-disable-next-line no-useless-escape
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 };
