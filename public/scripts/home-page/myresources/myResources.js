2ff45a6ddf6828800e7ee6fc0e6a91534b662a42
Jaybur1 <jayburbyga@gmail.com>
2020-04-07 12:54:03 -0400

    ui for myresources section

--- public/scripts/home-page/myresources/myResources.js
+++ public/scripts/home-page/myresources/myResources.js
@@ -12,16 +12,20 @@ const getUserResources = () => {
   });
 }
 
+const toggleTabs = () => {
+  $('.tabular.menu .item').tab();
+}
 const renderTabs = () => {
   const html = `
+<div class="ui conteiner">
 <div class="ui top attached tabular menu">
-  <div class="item active" data-tab="one"><i class="user tie icon"></i>Created</div>
-  <div class="item" data-tab="two"><i class="heart icon"></i>Liked</div>
-  <div class="item" data-tab="three"><i class="comment icon"></i>Commented</div>
-  <div class="item" data-tab="four"><i class="star icon"></i>Rated</div>
+  <div class="item tab active" data-tab="one"></a><i class="user tie icon"></i>Created</div>
+  <div class="item tab" data-tab="two"><i class="heart icon"></i>Liked</div>
+  <div class="item tab" data-tab="three"><i class="comment icon"></i>Commented</div>
+  <div class="item tab" data-tab="four"><i class="star icon"></i>Rated</div>
 </div>
 <div class="ui bottom attached tab segment active" data-tab="one">
-  <div class="user-resurces">No Resources yet ... <a>add</a> some to fill this section</div>
+  <div class="user-resurces">No Resources yet ... <button class="ui button pink create-new-resource">add</button> some to fill this section</div>
 </div>
 <div class="ui bottom attached tab segment" data-tab="two">
   <div class="liked-resurces">No Resources yet ... like some to fill this section</div>
@@ -32,6 +36,7 @@ const renderTabs = () => {
 <div class="ui bottom attached tab segment" data-tab="four">
   <div class="rated-resurces">No Resources yet ... rate some to fill this section</div>
 </div>
+</div>
 `;
 
 
@@ -41,8 +46,9 @@ const renderTabs = () => {
 
 const retrieveMyResources = () => {
   $(".my-resources-link").on("click", function() {
+    $('#home-page').html(renderTabs);
+    $('.tabular.menu .item').tab();
     toggleResourceMenu(this);
-    $('#home-page').html(renderTabs)
   });
 };
 
