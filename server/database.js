d7c07f73654c8e9fb1be996ccef1672a80504961
d0ugr <doug@escm.ml>
2020-04-07 09:51:51 -0400

    Refactor getResources

--- server/database.js
+++ server/database.js
@@ -95,295 +95,199 @@ const validatePassword = (db, userID, password) => {
 /////  RESOURCES  /////
 ///////////////////////
 
-// ! Function that retrieves resources from db based on various options --------------------------- //
-// Options:
-// {
-//   comments: true,
-//   likes: true,
-//   avgRatings: true,
-//   users: true,
-//   currentUser: "",
-//   categories: ["", "", ""],
-//   sort {
-//     byLatest: true,
-//     byOldest: true,
-//     byHighestRating: true,
-//     byLowestRating: true,
-//     byMostPopular: true,
-//     byLeastPopular: true,
-//   },
-//   limit: 0,
-//   filterByLiked: true,
-//   filterByCommented: true,
-//   filterByRated: true,
-// }
-
+// getResources retrieves resources from the database with many options.
+//
+//    options:
+//    {
+//      comments:    boolean,
+//      likes:       boolean,
+//      avgRatings:  boolean,
+//      users:       boolean,
+//      currentUser: string,
+//      categories:  [ string, string, string, ... ],
+//      sort {
+//        byLatest:        boolean,
+//        byOldest:        boolean,
+//        byHighestRating: boolean,
+//        byLowestRating:  boolean,
+//        byMostPopular:   boolean,
+//        byLeastPopular:  boolean,
+//      },
+//      filterByLiked:     boolean,
+//      filterByCommented: boolean,
+//      filterByRated:     boolean,
+//      limit: 0,
+//    }
+//
 // Examples
-// ? Landing-page popular resources simple
-// getResources(db, {likes: true, sorts: {byMostPopular: true}, limit: 8});
+//
+// Landing-page popular resources simple
+//
+//    getResources(db, { likes: true, sorts: { byMostPopular: true }, limit: 8 });
+//
+// My resources
+//
+//    getResources(db, { currentUser: "Ali", comments: true, likes: true });
+//
+// Liked, Rated, Commented resources (each can be on it's own)
+//
+//    getResources(db, { currentUser: "Alice", filterByLiked: true, filterByRated: true, filterByCommented: true });
+//
+// All resources, feed page (different sorts can be applied)
+//
+//    getResources(db, { likes: true, comments: true, avgRatings: true, sorts: { byLeastPopular: true } });
+//    getResources(db, { likes: true, comments: true, avgRatings: true, sorts: { byMostPopular: true } });
+//    getResources(db, { likes: true, comments: true, avgRatings: true, sorts: { byHighestRating: true } });
+//    getResources(db, { likes: true, comments: true, avgRatings: true, sorts: { byLowestRating: true } });
+//    getResources(db, { likes: true, comments: true, avgRatings: true, sorts: { byLatest: true, byHighestRating: true } });
+//    getResources(db, { likes: true, comments: true, avgRatings: true, sorts: { byOldest: true, byHighestRating: true } });
+//
+// Category or multiple categories (different sorts can be applied)
+//
+//    getResources(db, { categories: ["Art"]});
+//    getResources(db, { likes: true, avgRatings: true, categories: ["Art"] });
+//    getResources(db, { likes: true, avgRatings: true, categories: ["Art", "Tech"] });
+//    getResources(db, { likes: true, avgRatings: true, categories: ["Art", "Tech"], sorts: { byLeastPopular: true } });
+//    getResources(db, { likes: true, avgRatings: true, categories: ["Art", "Tech"], sorts: { byLowestRating: true } });
+//    getResources(db, { likes: true, avgRatings: true, categories: ["Art", "Tech"], sorts: { byHighestRating: true } });
+//
+// Categories for single user:
+//
+//    getResources(null, { likes: true, avgRatings: true, categories: ["Art", "Tech"], currentUser: "Ali" });
 
-// ? My resources
-// getResources(db, {currentUser: "Ali", comments: true, likes: true});
-
-// ? Liked, Rated, Commented resources (each can be on it's own)
-// getResources(db, {currentUser: "Alice", filterByLiked: true, filterByRated: true, filterByCommented: true});
-
-// ? All resources, feed page (different sorts can be applied)
-// getResources(db, {likes: true, comments: true, avgRatings: true, sorts: {byLeastPopular: true}});
-// getResources(db, {likes: true, comments: true, avgRatings: true, sorts: {byMostPopular: true}});
-// getResources(db, {likes: true, comments: true, avgRatings: true, sorts: {byHighestRating: true}});
-// getResources(db, {likes: true, comments: true, avgRatings: true, sorts: {byLowestRating: true}});
-// getResources(db, {likes: true, comments: true, avgRatings: true, sorts: {byLatest: true, byHighestRating: true}});
-// getResources(db, {likes: true, comments: true, avgRatings: true, sorts: {byOldest: true, byHighestRating: true}});
-
-// ? Category or multiple categories (different sorts can be applied)
-// getResources(db, { categories: ["Art"]});
-// getResources(db, {likes: true, avgRatings: true, categories: ["Art"]});
-// getResources(db, {likes: true, avgRatings: true, categories: ["Art", "Tech"]});
-// getResources(db, {likes: true, avgRatings: true, categories: ["Art", "Tech"], sorts: {byLeastPopular: true}});
-// getResources(db, {likes: true, avgRatings: true, categories: ["Art", "Tech"], sorts: {byLowestRating: true}});
-// getResources(db, {likes: true, avgRatings: true, categories: ["Art", "Tech"], sorts: {byHighestRating: true}});
-
-// ? Categories for single user
-// getResources(null, {likes: true, avgRatings: true, categories: ["Art", "Tech"], currentUser: "Ali"});
-// ! ---------------------------------------------------------------------------------------------------- //
 const getResources = (db, options) => {
+
   const queryParams = [];
 
-  // ? SELECT section of query
-  // Initialize query
-  let queryString = `SELECT resources.*`;
+  const selectUsers = options.users      || options.currentUser;
+  const joinRatings = options.avgRatings || options.ratings;
+  const joinUsers   = options.users      || (options.currentUser        &&
+                                             !options.filterByLiked     &&
+                                             !options.filterByCommented &&
+                                             !options.filterByRated);
 
-  // Comments are requested
-  options.comments ? (queryString += `, comments.body AS comments`) : null;
+  /////  SELECT  /////
 
-  // Likes are requested
-  options.likes ? (queryString += `, count(likes) AS likes`) : null;
+  let queryString = `SELECT resources.*` +
+                      `${options.comments   ? `, comments.body AS comments`          : ``}` +
+                      `${options.likes      ? `, COUNT(likes) AS likes`              : ``}` +
+                      `${options.avgRatings ? `, avg(ratings.rating) AS avg_ratings` : ``}` +
+                      `${selectUsers        ? `, users.name AS users`                : ``}` +
+                      `${options.categories ? `, categories.name AS categories`      : ``}` +
 
-  // Average ratings are requested
-  options.avgRatings
-    ? (queryString += `, avg(ratings.rating) AS avg_ratings`)
-    : null;
+  /////  FROM  /////
 
-  // Users or current user are requested
-  options.users || options.currentUser
-    ? (queryString += `, users.name AS users`)
-    : null;
+                    ` FROM resources` +
+                      `${options.comments   ? ` JOIN comments ON comments.resource_id = resources.id`     : ``}` +
+                      `${options.likes      ? ` JOIN likes ON likes.resource_id = resources.id`           : ``}` +
+                      `${joinRatings        ? ` JOIN ratings ON ratings.resource_id = resources.id`       : ``}` +
+                      `${joinUsers          ? ` JOIN users ON resources.user_id = users.id`               : ``}` +
+                      `${options.categories ? ` JOIN categories ON resources.category_id = categories.id` : ``}`;
 
-  // Categories are requested
-  options.categories
-    ? (queryString += `, categories.name AS categories`)
-    : null;
-  // ?
-
-  // ? FROM section of query
-  queryString += ` FROM resources `;
-  // ?
-
-  // ? JOIN section of query
-  // Comments are requested
-  options.comments
-    ? (queryString += `JOIN comments ON comments.resource_id = resources.id `)
-    : null;
-
-  // Likes are requested
-  options.likes
-    ? (queryString += `JOIN likes ON likes.resource_id = resources.id `)
-    : null;
-
-  // Average ratings or all ratings are requested
-  options.avgRatings || options.ratings
-    ? (queryString += `JOIN ratings ON ratings.resource_id = resources.id `)
-    : null;
-
-  // Users or current user are requested
-  options.users ||
-  (options.currentUser &&
-    !options.filterByLiked &&
-    !options.filterByCommented &&
-    !options.filterByRated)
-    ? (queryString += `JOIN users ON resources.user_id = users.id `)
-    : null;
-
-  // Categories are requested
-  options.categories
-    ? (queryString += `JOIN categories ON resources.category_id = categories.id `)
-    : null;
-
-  let alreadyFiltered = false;
-
-  // Liked by user
+  // users was joined above though...
+  let usersNotJoined = true;
   if (options.filterByLiked) {
-    queryString += `JOIN likes ON likes.resource_id = resources.id `;
-    alreadyFiltered
-      ? null
-      : (queryString += `JOIN users ON users.id = likes.user_id `);
-    alreadyFiltered = true;
+    queryString += ` JOIN likes ON likes.resource_id = resources.id` +
+                   ` JOIN users ON users.id = likes.user_id`;
+    usersNotJoined = false;
   }
-
-  // Commented by user
   if (options.filterByCommented) {
-    queryString += `JOIN comments ON comments.resource_id = resources.id `;
-    alreadyFiltered
-      ? null
-      : (queryString += `JOIN users ON users.id = comments.user_id `);
-    alreadyFiltered = true;
+    queryString += ` JOIN comments ON comments.resource_id = resources.id` +
+                   ` ${usersNotJoined ? `JOIN users ON users.id = comments.user_id` : ``}`;
+    usersNotJoined = false;
   }
-
-  // Rated by user
   if (options.filterByRated) {
-    queryString += `JOIN ratings ON ratings.resource_id = resources.id `;
-    alreadyFiltered
-      ? null
-      : (queryString += `JOIN users ON users.id = ratings.user_id `);
-    alreadyFiltered = true;
+    queryString += ` JOIN ratings ON ratings.resource_id = resources.id` +
+                   ` ${usersNotJoined ? `JOIN users ON users.id = ratings.user_id` : ``}`;
+    usersNotJoined = false;
   }
-  // ?
 
-  // ? WHERE section of query
-  let alreadyWhere = false;
+  /////  WHERE  /////
 
-  // Current user is requested
+  let addWhere = true;
+  // Filter by current user:
   if (options.currentUser) {
-    queryParams.push(`${options.currentUser}`);
-    queryString += ` WHERE users.name = $${queryParams.length} `;
-    alreadyWhere = true;
+    queryParams.push(`${options.currentUser}`.trim());
+    queryString += ` WHERE users.name = $${queryParams.length}`;
+    addWhere = false;
   }
-
-  // Categories requested
+  // Filter by categories:
   if (options.categories) {
-    // Can be filtered by multiple categories
-    options.categories.forEach((category, index) => {
+    options.categories.forEach((category, i) => {
       queryParams.push(`%${category}%`);
-
-      if (index === 0) {
-        alreadyWhere ? (queryString += ` AND `) : (queryString += ` WHERE `);
-      } else {
-        queryString += ` OR `;
-      }
-
-      queryString += `categories.name LIKE $${queryParams.length} `;
+      queryString += (i ? ` ${addWhere ? `WHERE` : `AND`}` : ` OR`) +
+                     ` categories.name LIKE $${queryParams.length}`;
+      addWhere = false;
     });
   }
-  // ?
 
-  // ? GROUP BY section of query
-  let alreadyGrouped = false;
+  /////  GROUP BY  /////
 
+  let addGroupBy = true;
   // Likes are requested
-  if (
-    options.likes &&
-    !options.filterByLiked &&
-    !options.filterByCommented &&
-    !options.filterByRated
-  ) {
+  if (options.likes &&
+      !options.filterByLiked && !options.filterByCommented && !options.filterByRated) {
     queryString += ` GROUP BY resources.id`;
-    alreadyGrouped = true;
+    addGroupBy = false;
   }
-
   // Comments are requested
-  if (
-    options.comments &&
-    !options.filterByLiked &&
-    !options.filterByCommented &&
-    !options.filterByRated
-  ) {
-    alreadyGrouped
-      ? (queryString += `, `)
-      : (queryString += ` GROUP BY resources.id, `);
-    queryString += `comments.body`;
-    alreadyGrouped = true;
+  if (options.comments &&
+      !options.filterByLiked && !options.filterByCommented && !options.filterByRated) {
+    queryString += `${addGroupBy ? ` GROUP BY resources.id, ` : `, `}comments.body`;
+    addGroupBy = false;
   }
-
   // Categories are requested
-  if (
-    options.categories &&
-    !options.filterByLiked &&
-    !options.filterByCommented &&
-    !options.filterByRated
-  ) {
-    alreadyGrouped
-      ? (queryString += `, `)
-      : (queryString += ` GROUP BY resources.id, `);
-    queryString += `categories.name `;
-    alreadyGrouped = true;
+  if (options.categories &&
+      !options.filterByLiked && !options.filterByCommented && !options.filterByRated) {
+    queryString += `${addGroupBy ? ` GROUP BY resources.id, ` : `, `}categories.name`;
+    addGroupBy = false;
   }
-
   // Users or current user are requested
-  if (
-    options.users ||
-    (options.currentUser &&
-      !options.filterByLiked &&
-      !options.filterByCommented &&
-      !options.filterByRated)
-  ) {
-    alreadyGrouped
-      ? (queryString += `, `)
-      : (queryString += ` GROUP BY resources.id, `);
-    queryString += `users.name `;
-    alreadyGrouped = true;
+  if (options.users ||
+      (options.currentUser &&
+       !options.filterByLiked && !options.filterByCommented && !options.filterByRated)) {
+    queryString += `${addGroupBy ? ` GROUP BY resources.id, ` : `, `}users.name`;
+    addGroupBy = false;
   }
-  // ?
 
-  // ? ORDER BY section of query
-  // Any sorts needed
+  /////  ORDER BY  /////
+
   if (options.sorts) {
-    queryString += ` ORDER BY `;
-
-    let alreadySorted = false;
-
-    // Sort by latest requested
+    const sortClause = [];
+    // Sort by timestamp:
     if (options.sorts.byLatest) {
-      queryString += `resources.created DESC`;
-      alreadySorted = true;
+      sortClause.push("resources.created DESC");
+    } else if (options.sorts.byOldest) {
+      sortClause.push("resources.created ASC");
     }
-
-    // Sort by oldest requested
-    if (options.sorts.byOldest) {
-      alreadySorted ? (queryString += `, `) : null;
-      queryString += `resources.created ASC`;
-      alreadySorted = true;
-    }
-
-    // Sort by highest average rating requested
+    // Sort by rating:
     if (options.sorts.byHighestRating) {
-      alreadySorted ? (queryString += `, `) : null;
-      queryString += `avg(ratings.rating) DESC`;
-      alreadySorted = true;
+      sortClause.push("AVG(ratings.rating) DESC");
+    } else if (options.sorts.byLowestRating) {
+      sortClause.push("AVG(ratings.rating) ASC");
     }
-
-    // Sort by lowest average rating requested
-    if (options.sorts.byLowestRating) {
-      alreadySorted ? (queryString += `, `) : null;
-      queryString += `avg(ratings.rating) ASC`;
-      alreadySorted = true;
-    }
-
-    // Sort by most popular requested
+    // Sort by popularity:
     if (options.sorts.byMostPopular) {
-      alreadySorted ? (queryString += `, `) : null;
-      queryString += `likes DESC`;
-      alreadySorted = true;
+      sortClause.push("likes DESC");
+    } else if (options.sorts.byLeastPopular) {
+      sortClause.push("likes ASC");
     }
-
-    // Sort by least popular requested
-    if (options.sorts.byLeastPopular) {
-      alreadySorted ? (queryString += `, `) : null;
-      queryString += `likes ASC`;
-      alreadySorted = true;
+    // Add sort clause if there is anything to sort by:
+    if (sortClause.length > 0) {
+      queryString += ` ORDER BY ${sortClause.join(", ")}`;
     }
   }
-  // ?
 
-  // ? Limit section of query
-  queryParams.push(Number(options.limit) || 10);
-  queryString += ` LIMIT $${queryParams.length};`;
-  // ?
+  /////  LIMIT  /////
 
+  queryString += ` LIMIT ${Number(options.limit) || 10}`;
+
+  // OMG let's execute the thing and see what happens!
   return db
     .query(queryString, queryParams)
     .then((res) => res.rows)
     .catch((err) => console.error("getResources error:", err));
+
 };
 
 // addResource adds a new resource to the database.
