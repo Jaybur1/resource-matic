60119b72dc5929c8e399860f9fb5f7de68b1a61c
d0ugr <doug@escm.ml>
2020-04-08 00:21:24 -0400

    Add datestyle

--- db/seeds/03_resources.sql
+++ db/seeds/03_resources.sql
@@ -1,6 +1,9 @@
 -- Resources table seeds here
+
+SET datestyle = 'mdy';
+
 INSERT INTO resources
-  (user_id,category_id,title,description,content,thumbnail_photo, created)
+  (user_id, category_id, title, description, content, thumbnail_photo, created)
 VALUES
   (1, 1, 'CSS Tricks', 'Lets build a literal grid of squares, and well put the logos of some magazines centered inside each square. I imagine plenty of you have had to build a logo grid before.', 'https://css-tricks.com/', 'https://i0.wp.com/css-tricks.com/wp-content/uploads/2019/06/akqRGyta_400x400.jpg?ssl=1', '02/22/2020 04:54:58 PM'),
   (2, 2, 'Netlify', 'Build, test, and deploy globally with Netlify’s all-in-one platform for modern web projects', 'https://www.netlify.com/', 'https://www.netlify.com/img/press/logos/logomark.png', '10/07/2019 08:52:25 PM'),
