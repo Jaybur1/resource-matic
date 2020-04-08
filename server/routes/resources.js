3a6b0e79f137c890a74499fce2f5901a4f9fb87d
d0ugr <doug@escm.ml>
2020-04-08 12:42:54 -0400

    Initial search implementation

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -10,26 +10,31 @@ const database = require("../database");
 
 const resourcesRoutes = (db) => {
 
-  // Handle request resources
+  // TODO: Add error handling
+
+  // Request resources
   router.get("/", (req, res) => {
 
     if (req.query.currentUser) {
-      const user_id = req.session.userId;
-      console.log(user_id);
-      database.getResources(db, {...req.query, currentUser: Number(user_id)})
-        .then((queryRes) => {
-          console.log(queryRes)
-          res.json(queryRes)});
+      const userId = req.session.userId;
+      database.getResources(db, {...req.query, currentUser: Number(userId)})
+        .then((queryRes) => res.json(queryRes));
     } else {
       database.getResources(db, req.query)
         .then((queryRes) => res.json(queryRes));
     }
   });
 
-  // Handle create new resource
+  // Create a new resource
   router.post("/", (req, res) => {
     database.addResource({ userId: req.session.userId, ...req.body }, db)
-      .then(resource => res.send(resource));
+      .then((resource) => res.send(resource));
+  });
+
+  // Search resources
+  router.get("/search", (req, res) => {
+    database.searchResources(db, req.query.searchText)
+      .then((results) => res.json(results));
   });
 
   return router;
