157b83ed3c4c6a2956df90fa975fe785a9650c3e
aliyasser20 <aliyasser20@hotmail.com>
2020-04-09 00:48:19 -0400

    simulate min delay with placeholders

--- public/scripts/home-page/browse/browse.js
+++ public/scripts/home-page/browse/browse.js
@@ -4,6 +4,7 @@ import { likeInteractions } from "../feed/like.js";
 import { ratingInteractions } from "../feed/rating.js";
 import { groupComments } from "../feed/feed.js";
 import { getCategories, capitalize } from "../../new-resource.js";
+import { createPlaceholderCards } from "../../landing-page/placeholder-cards.js";
 
 // Function that creates and renders browse page
 const retrieveBrowseResources = async() => {
@@ -96,26 +97,43 @@ const retrieveBrowseCards = (categories) => {
 const cardsRenderer = async(resources) => {
   // Clear main area of home page
   $(".user-resources").empty();
-  // Render feed
-  $(".user-resources").append(await cardsCreator(resources));
-  // Handles clicking resources
-  handleClickedResource();
-  // Event listener for view more comments
-  showMoreComments(3);
-  // Event listener for new comment
-  newComment();
-  // Event listener for edit comment
-  editComment();
-  // Event listener for edit comment
-  deleteComment();
-  // Event listener for like click
-  likeInteractions();
-  // Event listener for like rating
-  ratingInteractions();
+  // Append placeholder cards
+  $(".user-resources").append(createPlaceholderCards());
 
-  $(".special.cards .image").dimmer({
-    on: "hover",
-  });
+  // Simulate minimum delay of 1s for placeholders
+  setTimeout(async() => {
+    // Get cards
+    const cardsBack = await cardsCreator(resources);
+    // Clear main area of home page
+    $(".user-resources").empty();
+    
+    // If cards are present render them. Otherwise render no results found
+    if (cardsBack.length > 0) {
+      $(".user-resources").append(await cardsCreator(resources));
+    } else {
+      $(".user-resources").append(`<div class="custom-nothing-matched"><h3>No results found <br> Please select another category</h3></div> `);
+    }
+    
+    // Handles clicking resources
+    handleClickedResource();
+    // Event listener for view more comments
+    showMoreComments(3);
+    // Event listener for new comment
+    newComment();
+    // Event listener for edit comment
+    editComment();
+    // Event listener for edit comment
+    deleteComment();
+    // Event listener for like click
+    likeInteractions();
+    // Event listener for like rating
+    ratingInteractions();
+
+    $(".special.cards .image").dimmer({
+      on: "hover",
+    });
+  }, 1000);
+
 };
 
 // Function that creates cards html
