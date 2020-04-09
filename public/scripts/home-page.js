2b53864f4b80efb5606f2cb2d67cd5a9ba3147fc
aliyasser20 <aliyasser20@hotmail.com>
2020-04-09 10:40:57 -0400

    responsive home menu

--- public/scripts/home-page.js
+++ public/scripts/home-page.js
@@ -6,12 +6,26 @@ import retrieveFeedResources from "./home-page/feed/feed.js";
 import toggleResourceMenu from "./home-page/home-page-helpers/toggleRescourseMenu.js";
 
 $(document).ready(() => {
+  // Handle width
+  widthHandle();
   // Scroll to top of page
-  retrieveFeedResources();
-  toggleResourceMenu();
   window.scroll(0, 0);
+  // Render feed
+  retrieveFeedResources();
+  // Event listener for menu
+  toggleResourceMenu();
 
   $(window).on("resize", function() {
     console.log($(window).width());
+
+    widthHandle();
   });
 });
+
+const widthHandle = () => {
+  if ($(window).width() >= 992) {
+    $(".custom-right-menu").addClass("vertical");
+  } else {
+    $(".custom-right-menu").removeClass("vertical");
+  }
+};
