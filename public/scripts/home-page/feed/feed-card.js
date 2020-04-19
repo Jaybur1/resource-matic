e6cd4cd7cc8a78b6545b23794dd1cf5d87078d52
Jaybur1 <jayburbyga@gmail.com>
2020-04-19 14:13:49 -0400

    add escape xss

--- public/scripts/home-page/feed/feed-card.js
+++ public/scripts/home-page/feed/feed-card.js
@@ -6,24 +6,35 @@ import { createCommentsHTML } from "./comments.js";
 import { checkIfLiked } from "./like.js";
 import { checkIfRated } from "./rating.js";
 
+const escapeXSS = str => {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 // Function that creates single cards for feed
 const feedCardCreator = async(resource) => {
   // Card html
+
+  const poster = escapeXSS(resource.poster);
+  const content = escapeXSS(resource.content);
+  const title = escapeXSS(resource.title);
+  const description = escapeXSS(resource.description);
   const  cardHTML = `
     <article class="ui fluid card custom-big-card" >
     
     <div class="content">
       <div class="right floated meta">${$.timeago(resource.created)}</div>
-      <img class="ui avatar image" src="${resource.poster_avatar}"> &nbsp <span class="custom-poster-name">${resource.poster}</span>
+      <img class="ui avatar image" src="${resource.poster_avatar}"> &nbsp <span class="custom-poster-name">${poster}</span>
     </div>
     <div class=" content custom-resource-area"> 
-      <a class="custom-image-link" href="${resource.content}" target="_blank">
+      <a class="custom-image-link" href="${content}" target="_blank">
         <div class="custom-image-hover"></div>
         <img class="custom-resource-image" src="${resource.thumbnail_photo}">
       </a>
       <div class="custom-resource-name">
-        <a href="${resource.content}" target="_blank">${resource.title}</a>
-        <span class="custom-resource-description">${resource.description}</span>
+        <a href="${resource.content}" target="_blank">${title}</a>
+        <span class="custom-resource-description">${description}</span>
       </div>
       <div class=" custom-rating">
       <span> 
